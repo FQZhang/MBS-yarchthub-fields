a6214b210c9fe4d89c5b60ae21d21bb74b18ef8f
Chris Seufert <chris@seufert.id.au>
2019-01-30 10:52:39 +1100

    Code to inject JS into editor page.

---
+++ inject.js
@@ -0,0 +1,1 @@
+(function() {var el = document.createElement('script'); el.type='text/javascript'; el.src='https://melbourneboatsales_com_au.myudo.net/_shim/customfields.js'; document.body.appendChild(el)})()
